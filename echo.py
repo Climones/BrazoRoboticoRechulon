ab806d5f3f0c42d41c6387d083430796810cf194
Climones <65442692+Climones@users.noreply.github.com>
2023-11-15 20:37:29 +0100

    Update echo.py

--- echo.py
+++ echo.py
@@ -1,51 +1,48 @@
-//Conexión to guapa al  ESP-8266
-
-//Importamos librerías
-#include "ESP8266WiFi.h"
-
-//Variables
-  const char* ssid = "ESIBot";
-  const char* password = "AlbaritoWelb?";
-  //Objeto WiFiServer Como parámetro se pasa el puerto al que estÃ¡ conectado
-  WiFiServer Server(80);
-  String msg;
-
-  
-
-void setup() {
-    Serial.begin(115200);
-    delay(1000);
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    if(WiFi.status() == WL_NO_SSID_AVAIL){
-    Serial.println("Error con el nombre del punto de acceso");
- }else if(WiFi.status() == WL_CONNECT_FAILED){
-          Serial.println("Error en la contraseÃ±a");
- }else{
-    Serial.println("Conectando..");
-  }
- }
- Serial.print("Conectado a WiFi. IP:");
- Serial.println(WiFi.localIP());
- Server.begin();
-}
-void loop() {
-WiFiClient client = Server.available();
- if (client) {
- while (client.connected()) {
- while (client.available()>0) {
- char c = client.read();
- Serial.write(c);
- msg =msg+c;
- }
- if(msg!=""){
- client.print("este es su mensaje:"+msg);
- msg="";
- }
- delay(10);
- }
- client.stop();
- Serial.println("Client disconnected");
- }
-}
+#Incluimos las librerías
+import socket
+import sys
+#Creamos el socket de comunicación
+try:
+ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error as e:
+ print ('Fallo al crear socket')
+ sys.exit()
+print ('Socket Creado')
+#Creamos las variables para comunicarnos
+IP = '192.168.2.117'
+puerto = 80
+#Intentamos iniciar la comunicación
+try:
+ s.connect((IP,puerto))
+except socket.error as e:
+ print ('Fallo al conectar')
+ s.close()
+ sys.exit()
+print ('Conectado')
+#Variable para mantener el bucle while
+Salida = True
+#Bucle while sale cuando se introduce por teclado Adios
+while Salida:
+ #Solicita ingresar un mensaje para enviar
+ print ('Introduzca mensaje')
+ msg = input()
+ #Comprueba si el mensaje es Adios
+ if msg=='Adios':
+ #Si lo es configura la salida del bucle
+  Salida = False
+ #Envía el mensaje
+ try:
+ #Codifica antes de enviar
+  s.sendall(msg.encode())
+ except socket.error as e:
+  print ('error al enviar el mensaje')
+  s.close()
+  sys.exit()
+ print('Mensaje enviado exitosamente')
+ #Recibe la respuesta
+ respuesta = s.recv(1024)
+ print (respuesta.decode())
+#Finaliza el bucle, cierra los sockets y cierra el programa
+s.close()
+print ('Socket cerrado. Saliendo...')
+sys.exit()
